4d9c26257093a58036b48b0870128175f2cccac0
Tom Kay <oridan82@gmail.com>
2025-03-26 11:18:08 +0000

    add BasicMask for PII

--- keystone/type_pii.go
+++ keystone/type_pii.go
@@ -1,7 +1,9 @@
 package keystone
 
 import (
+	"math/rand/v2"
 	"net"
+	"strings"
 
 	"github.com/keystonedb/sdk-go/proto"
 )
@@ -49,7 +51,7 @@ func NewSecurePII(info, masked string) SecurePII {
 type PersonName struct{ SecureString }
 
 func NewPersonName(name string) PersonName {
-	mask := name // TODO: MASK
+	mask := BasicMask(name)
 	return PersonName{NewSecureString(name, mask)}
 }
 
@@ -87,4 +89,20 @@ func (s *SecureIP) PropertyDefinition() proto.PropertyDefinition {
 
 func NewSecureIPV4(ip string) SecureIP {
 	return SecureIP{NewSecureString(ip, net.ParseIP(ip).Mask(net.IPv4Mask(0xff, 0xff, 0xff, 0)).String())}
+}
+
+func BasicMask(unmasked string) string {
+	// split masked into words
+	words := strings.Split(unmasked, " ")
+	for i, word := range words {
+		word = strings.TrimSpace(word)
+		// if greater than 2 characters, put 3-7 asterisks between the first and last letter
+		if l := len(word); l > 2 {
+			words[i] = word[:1] + strings.Repeat("*", rand.IntN(7)+3) + word[l-1:]
+		} else if l > 1 {
+			// whole word becomes 3-7 asterisks
+			words[i] = word[:1] + strings.Repeat("*", rand.IntN(7)+3)
+		}
+	}
+	return strings.Join(words, " ")
 }
