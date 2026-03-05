1e1826422da323a1c54d1b54e122c443a5e39cd6
Brooke Bryan <brooke@bajb.net>
2026-03-05 14:15:09 +0000

    Additional mock funcs

--- keystone/mock.go
+++ keystone/mock.go
@@ -17,10 +17,14 @@ type MockServer struct {
 	proto.UnimplementedKeystoneServer
 	DefineFunc           func(context.Context, *proto.SchemaRequest) (*proto.Schema, error)
 	MutateFunc           func(context.Context, *proto.MutateRequest) (*proto.MutateResponse, error)
+	DestroyFunc          func(context.Context, *proto.DestroyRequest) (*proto.DestroyResponse, error)
 	ReportTimeSeriesFunc func(context.Context, *proto.ReportTimeSeriesRequest) (*proto.MutateResponse, error)
+	SnapshotReportFunc   func(context.Context, *proto.SnapshotReportRequest) (*proto.MutateResponse, error)
 	RetrieveFunc         func(context.Context, *proto.EntityRequest) (*proto.EntityResponse, error)
 	FindFunc             func(context.Context, *proto.FindRequest) (*proto.FindResponse, error)
 	ListFunc             func(context.Context, *proto.ListRequest) (*proto.ListResponse, error)
+	QueryIndexFunc       func(context.Context, *proto.QueryIndexRequest) (*proto.QueryIndexResponse, error)
+	LookupFunc           func(context.Context, *proto.LookupRequest) (*proto.LookupResponse, error)
 	GroupCountFunc       func(context.Context, *proto.GroupCountRequest) (*proto.GroupCountResponse, error)
 	LogFunc              func(context.Context, *proto.LogRequest) (*proto.LogResponse, error)
 	LogsFunc             func(context.Context, *proto.LogsRequest) (*proto.LogsResponse, error)
@@ -32,6 +36,16 @@ type MockServer struct {
 	SharedViewsFunc      func(context.Context, *proto.SharedViewsRequest) (*proto.SharedViewsResponse, error)
 	RateLimitFunc        func(context.Context, *proto.RateLimitRequest) (*proto.RateLimitResponse, error)
 	StatusFunc           func(context.Context, *proto.Authorization) (*proto.StatusResponse, error)
+	IIDFunc              func(context.Context, *proto.IIDCreateRequest) (*proto.IIDResponse, error)
+	IIDLookupFunc        func(context.Context, *proto.IIDRequest) (*proto.IIDsResponse, error)
+	PiiTokenFunc         func(context.Context, *proto.PiiTokenRequest) (*proto.PiiTokenResponse, error)
+	PiiAnonymizeFunc     func(context.Context, *proto.PiiAnonymizeRequest) (*proto.PiiAnonymizeResponse, error)
+	SquidFunc            func(context.Context, *proto.SquidRequest) (*proto.SquidResponse, error)
+	SquidRecoverFunc     func(context.Context, *proto.SquidRecoverRequest) (*proto.SquidResponse, error)
+	AKVGetFunc           func(context.Context, *proto.AKVGetRequest) (*proto.AKVGetResponse, error)
+	AKVPutFunc           func(context.Context, *proto.AKVPutRequest) (*proto.GenericResponse, error)
+	AKVDelFunc           func(context.Context, *proto.AKVDelRequest) (*proto.GenericResponse, error)
+	PushTaskFunc         func(context.Context, *proto.PushTaskRequest) (*proto.GenericResponse, error)
 }
 
 func bufDialer(context.Context, string) (net.Conn, error) {
@@ -102,7 +116,7 @@ func (m *MockServer) GroupCount(ctx context.Context, req *proto.GroupCountReques
 }
 
 func (m *MockServer) Log(ctx context.Context, req *proto.LogRequest) (*proto.LogResponse, error) {
-	if m.LogsFunc == nil {
+	if m.LogFunc == nil {
 		return m.UnimplementedKeystoneServer.Log(ctx, req)
 	}
 	return m.LogFunc(ctx, req)
@@ -156,7 +170,7 @@ func (m *MockServer) ShareViews(ctx context.Context, req *proto.SharedViewsReque
 }
 
 func (m *MockServer) RateLimit(ctx context.Context, req *proto.RateLimitRequest) (*proto.RateLimitResponse, error) {
-	if m.SharedViewsFunc == nil {
+	if m.RateLimitFunc == nil {
 		return m.UnimplementedKeystoneServer.RateLimit(ctx, req)
 	}
 	return m.RateLimitFunc(ctx, req)
@@ -167,4 +181,102 @@ func (m *MockServer) Status(ctx context.Context, req *proto.Authorization) (*pro
 		return m.UnimplementedKeystoneServer.Status(ctx, req)
 	}
 	return m.StatusFunc(ctx, req)
+}
+
+func (m *MockServer) Destroy(ctx context.Context, req *proto.DestroyRequest) (*proto.DestroyResponse, error) {
+	if m.DestroyFunc == nil {
+		return m.UnimplementedKeystoneServer.Destroy(ctx, req)
+	}
+	return m.DestroyFunc(ctx, req)
+}
+
+func (m *MockServer) SnapshotReport(ctx context.Context, req *proto.SnapshotReportRequest) (*proto.MutateResponse, error) {
+	if m.SnapshotReportFunc == nil {
+		return m.UnimplementedKeystoneServer.SnapshotReport(ctx, req)
+	}
+	return m.SnapshotReportFunc(ctx, req)
+}
+
+func (m *MockServer) QueryIndex(ctx context.Context, req *proto.QueryIndexRequest) (*proto.QueryIndexResponse, error) {
+	if m.QueryIndexFunc == nil {
+		return m.UnimplementedKeystoneServer.QueryIndex(ctx, req)
+	}
+	return m.QueryIndexFunc(ctx, req)
+}
+
+func (m *MockServer) Lookup(ctx context.Context, req *proto.LookupRequest) (*proto.LookupResponse, error) {
+	if m.LookupFunc == nil {
+		return m.UnimplementedKeystoneServer.Lookup(ctx, req)
+	}
+	return m.LookupFunc(ctx, req)
+}
+
+func (m *MockServer) IID(ctx context.Context, req *proto.IIDCreateRequest) (*proto.IIDResponse, error) {
+	if m.IIDFunc == nil {
+		return m.UnimplementedKeystoneServer.IID(ctx, req)
+	}
+	return m.IIDFunc(ctx, req)
+}
+
+func (m *MockServer) IIDLookup(ctx context.Context, req *proto.IIDRequest) (*proto.IIDsResponse, error) {
+	if m.IIDLookupFunc == nil {
+		return m.UnimplementedKeystoneServer.IIDLookup(ctx, req)
+	}
+	return m.IIDLookupFunc(ctx, req)
+}
+
+func (m *MockServer) PiiToken(ctx context.Context, req *proto.PiiTokenRequest) (*proto.PiiTokenResponse, error) {
+	if m.PiiTokenFunc == nil {
+		return m.UnimplementedKeystoneServer.PiiToken(ctx, req)
+	}
+	return m.PiiTokenFunc(ctx, req)
+}
+
+func (m *MockServer) PiiAnonymize(ctx context.Context, req *proto.PiiAnonymizeRequest) (*proto.PiiAnonymizeResponse, error) {
+	if m.PiiAnonymizeFunc == nil {
+		return m.UnimplementedKeystoneServer.PiiAnonymize(ctx, req)
+	}
+	return m.PiiAnonymizeFunc(ctx, req)
+}
+
+func (m *MockServer) SQUID(ctx context.Context, req *proto.SquidRequest) (*proto.SquidResponse, error) {
+	if m.SquidFunc == nil {
+		return m.UnimplementedKeystoneServer.SQUID(ctx, req)
+	}
+	return m.SquidFunc(ctx, req)
+}
+
+func (m *MockServer) SQUIDRecover(ctx context.Context, req *proto.SquidRecoverRequest) (*proto.SquidResponse, error) {
+	if m.SquidRecoverFunc == nil {
+		return m.UnimplementedKeystoneServer.SQUIDRecover(ctx, req)
+	}
+	return m.SquidRecoverFunc(ctx, req)
+}
+
+func (m *MockServer) AKVGet(ctx context.Context, req *proto.AKVGetRequest) (*proto.AKVGetResponse, error) {
+	if m.AKVGetFunc == nil {
+		return m.UnimplementedKeystoneServer.AKVGet(ctx, req)
+	}
+	return m.AKVGetFunc(ctx, req)
+}
+
+func (m *MockServer) AKVPut(ctx context.Context, req *proto.AKVPutRequest) (*proto.GenericResponse, error) {
+	if m.AKVPutFunc == nil {
+		return m.UnimplementedKeystoneServer.AKVPut(ctx, req)
+	}
+	return m.AKVPutFunc(ctx, req)
+}
+
+func (m *MockServer) AKVDel(ctx context.Context, req *proto.AKVDelRequest) (*proto.GenericResponse, error) {
+	if m.AKVDelFunc == nil {
+		return m.UnimplementedKeystoneServer.AKVDel(ctx, req)
+	}
+	return m.AKVDelFunc(ctx, req)
+}
+
+func (m *MockServer) PushTask(ctx context.Context, req *proto.PushTaskRequest) (*proto.GenericResponse, error) {
+	if m.PushTaskFunc == nil {
+		return m.UnimplementedKeystoneServer.PushTask(ctx, req)
+	}
+	return m.PushTaskFunc(ctx, req)
 }
