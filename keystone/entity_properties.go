e8ce255796b4e99f967f2533506af7c8e0c070e3
Tom Kay <oridan82@gmail.com>
2026-04-24 14:46:49 +0100

    add internal property helpers

---
+++ keystone/entity_properties.go
@@ -0,0 +1,8 @@
+package keystone
+
+const PropertyEntityID = "_eid"
+const PropertyChildID = "_cid"
+const PropertyTimeID = "_tid"
+const PropertyCreated = "_created"
+const PropertyLastUpdate = "_last_update"
+const PropertyState = "_state"
